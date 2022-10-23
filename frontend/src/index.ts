b02dbf6885629a2bba7ac9a623ad20066084e975
cilinder <jure.taslak@gmail.com>
2022-10-23 16:40:04 +0200

    WIP styles rework, use tailwind css

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,3 +1,10 @@
 export function test() {
   console.log("Hello");
 }
+
+export function askForDelete(recipeId: number) {
+  console.log("Deleting recipe: ", recipeId);
+  if (confirm("Are you sure you want to delete this recipe?!!")) {
+    window.location.href=`/recipes/delete/${recipeId}`;
+  }
+}
